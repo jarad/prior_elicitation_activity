68564fb5c989ea19b4f727a978e387eb6b4d2048
Jarad Niemi <niemi@iastate.edu>
2016-01-25 15:36:21 -0600

    Swap order of prior plots

--- server.R
+++ server.R
@@ -17,15 +17,6 @@ shinyServer(function(input,output,session) {
     
     par(mfrow=c(1,2))
     
-    curve(dsqrtinvgamma(x, input$v/2, input$v*input$s^2/2), 
-          from = 0, 
-          to   = input$s*4,
-          main = "Prior on height standard deviation",
-          xlab = "Height standard deviation",
-          ylab = "Density for height standard deviation",
-          lwd  = 2,
-          col  = "gray")
-    
     curve(ns_dt(x, input$v, input$m, input$s/sqrt(input$k)), 
           from = input$m-input$s/sqrt(input$k)*qt(.99, input$v), 
           to   = input$m+input$s/sqrt(input$k)*qt(.99, input$v),
@@ -33,6 +24,15 @@ shinyServer(function(input,output,session) {
           main = "Prior on mean height",
           xlab = "Mean height", 
           ylab = "Density for mean height",
+          lwd  = 2,
+          col  = "gray")
+    
+    curve(dsqrtinvgamma(x, input$v/2, input$v*input$s^2/2), 
+          from = 0, 
+          to   = input$s*4,
+          main = "Prior on height standard deviation",
+          xlab = "Height standard deviation",
+          ylab = "Density for height standard deviation",
           lwd  = 2,
           col  = "gray")
     
